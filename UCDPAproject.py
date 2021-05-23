0c2a2695bf52859a3578bfa96aa76de0f7c21f59
Niamh O'Mahony <niamhomahony966@gmail.com>
2021-05-23 13:33:54 +0100

    Cleaning data

--- UCDPAproject.py
+++ UCDPAproject.py
@@ -2,7 +2,6 @@
 """
 Spyder Editor
 
-This is a temporary script file.
 """
 import pandas as pd
 import numpy as np
@@ -12,8 +11,10 @@ import matplotlib.pyplot as plt
 
 file = "C:\\Users\\omaho\\downloads\\public-register-03052021.xlsx"
 charity_df = pd.ExcelFile(file)
+
 # get the sheet names
 print(charity_df.sheet_names)
+
 # split into 2 dfs
 pr_df = charity_df.parse('Public Register', skiprows=1)
 ar_df = charity_df.parse('Annual Reports', skiprows=1)
@@ -22,11 +23,30 @@ print(ar_df.head())
 
 # Set indexes to Registered Charity Number in both
 prind_df=pr_df.set_index('Registered Charity Number')
-arind_df=ar_df.set_index('Registered Charity Number')
+arind_df=ar_df.set_index('Period End Date')
 
 # check number of nulls
 prind_df.isnull().sum()
 arind_df.isnull().sum()
+
+
+# Clean prind_df
+dictpr = {'Primary Address': 'Not given', 'CRO Number':'Not Registered', 'CHY Number': 'Not Registered', 'Charitable Purpose': 'Not given', 'Charitable Objects': 'Not given'} 
+prind_df=prind_df.fillna(dictpr)
+
 # AR get rid of the empty columns
 arind_df=arind_df.dropna(axis =1, thresh = 10000)
+print(arind_df.shape)
+# Drop rows with no Financial Data
+
+# select report dates in 2019 only
+
+ar19ind_df=arind_df.loc['2019-01-01':'2019-12-31']
+print(ar19ind_df.head())
+print(ar19ind_df.shape)
+
+# Change index of Annual Report df to Registered Charity Number
+
+ar19ind_df = ar19ind_df.set_index('Registered Charity Name')
+
 
